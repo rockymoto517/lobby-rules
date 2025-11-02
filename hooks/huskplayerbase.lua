e875341fa173e07897e144864fc034f5f81978fe
rockymoto517 <rockymoto517@gmail.com>
2025-11-02 00:35:21 -0500

    cc check fix

--- hooks/huskplayerbase.lua
+++ hooks/huskplayerbase.lua
@@ -3,8 +3,17 @@ function HuskPlayerBase:upgrade_value(category, upgrade)
 	local val = old_upgrade_value(self, category, upgrade)
 
 	-- Crew chief check
+	-- Lots of extra checks cause it's needed
 	local peer = managers.network:session():peer_by_unit(self._unit)
-	local deck_id = string.split(string.split(peer:skills(), "-")[2], "_")
+	local deck_id = 0
+	if peer then
+		local skills_deck_info = string.split(peer:skills(), "-")
+		if skills_deck_info then
+			local deck = skills_deck_info[2]
+			deck_id = number(string.split(deck, "_")[1])
+		end
+	end
+
 	if LobbyRules.settings.no_second_joker and deck_id ~= 1 then
 		if category == "player" and upgrade == "convert_enemies_max_minions" then
 			val = val and 1 or 0
